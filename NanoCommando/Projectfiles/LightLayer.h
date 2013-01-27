91c745a75441156e140f46992ee5ab8231ab0a18
mocxd <mark.adkins.hastings@gmail.com>
2013-01-27 03:20:27 -0500

    added dark space

---
+++ NanoCommando/Projectfiles/LightLayer.h
@@ -0,0 +1,13 @@
+//
+//  LightLayer.h
+//  NanoCommando
+//
+//  Created by Mark Adkins-hastings on 1/27/13.
+//
+//
+
+#import "CCLayer.h"
+
+@interface LightLayer : CCLayer
+
+@end
---
+++ NanoCommando/Projectfiles/LightLayer.m
@@ -0,0 +1,13 @@
+//
+//  LightLayer.m
+//  NanoCommando
+//
+//  Created by Mark Adkins-hastings on 1/27/13.
+//
+//
+
+#import "LightLayer.h"
+
+@implementation LightLayer
+
+@end
